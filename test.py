7b8d9c9baae828fa0412809dc50db9479f239238
Lorpaves <kirass1230@163.com>
2022-12-09 15:10:33 +0800

    delete unnecessary file

--- test.py
+++
@@ -1,7 +0,0 @@
-import translators as ts
-import translators.server as tss
-import pysubs2 as pysub
-sub = pysub.load('./2.srt')
-sub_string = sub.to_string('srt')
-ts_test = tss.translate_text(
-    query_text=sub_string, translator='google', from_language='en', to_language='zh')
